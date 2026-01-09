20a96525cd8408fe3f6232d08d0105a3575d5269
Mohit25f101 <25f1001180@ds.study.iitm.ac.in>
2026-01-10 01:31:59 +0530

    A

--- script.js
+++ script.js
@@ -100,7 +100,7 @@ async function analyzeAudio(file) {
 
     try {
         // --- THE KEY FIX: Explicit IP and Port ---
-        const response = await fetch("http://127.0.0.1:8000/analyze", {
+        const response = await fetch("http://127.0.0.1:8001/analyze", {
             method: "POST",
             body: formData
         });
